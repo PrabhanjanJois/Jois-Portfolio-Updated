9e23a05c7440ab3b2bd47f3a696151bba60ea205
Prabhanjan-CC <prabhanjan.jois@cognitiveclouds.com>
2024-12-15 16:20:40 +0530

    main push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,29 @@
 import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  description: "My Portfolio for my skills display",
 };
-
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+      <html lang="en">
+      <body
+          className={`${inter.variable} ${calistoga.variable} bg-gray-900 text-white antialiased font-sans`}
+      >
+      {children}
+      </body>
+      </html>
   );
 }
